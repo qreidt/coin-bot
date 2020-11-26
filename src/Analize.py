a277ff46a485bf9a54bce943b7fc3105cc567faa
Caio Reidt <caio.reidt@garnon.me>
2020-11-26 01:17:08 -0300

    Indicador de RSI

--- src/Analize.py
+++ src/Analize.py
@@ -8,9 +8,13 @@ cg = CoinGeckoAPI()
 
 coins = pd.read_csv(DATA_PATH + 'coins.csv', sep=';')
 
-for index, coin in coins[coins['id'] == 'brz'].iterrows():
+# for index, coin in coins[coins['id'] == 'brz'].iterrows():
+for index, coin in coins.iterrows():
+
+	print(str(index + 1) + '/' + str(len(coins)) + ' | ' + coin['name'])
 
 	history = pd.read_csv(coin['path'] + 'history.csv', sep=';')
 
 	ic.writeSMA(history).to_csv(coin['path'] + 'sma.csv', sep=';', index=False)
+	ic.writeRSI(history).to_csv(coin['path'] + 'rsi.csv', sep=';', index=False)
 
--- src/controllers/IndicatorController.py
+++ src/controllers/IndicatorController.py
@@ -11,6 +11,42 @@ def writeSMA(coin_history):
 	coin_history['SMA100'] = coin_history['value'].rolling(window=100).mean()
 	coin_history['SMA200'] = coin_history['value'].rolling(window=200).mean()
 
-	del coin_history['value']
+	return coin_history
+
+
+# CALCULAR RSI
+# ÍNDICE DE FORÇA RELATIVA - RELATIVE STRENGTH INDEX
+def writeRSI(coin_history, algorithm='sma'):
+
+	coin_history['U'] = coin_history['value'].diff()
+	coin_history['U'] = coin_history['U'].mask(coin_history['U'].lt(0), 0)
+	coin_history['D'] = -coin_history['value'].diff()
+	coin_history['D'] = coin_history['D'].mask(coin_history['D'].lt(0), 0)
+
+	N = 14
+
+	# SMA14
+	if algorithm == 'sma':
+		coin_history['RSI-AVG-U'] = coin_history['U'].rolling(window=N).mean()
+		coin_history['RSI-AVG-D'] = coin_history['D'].rolling(window=N).mean()
+		coin_history['RSI-RS'] = coin_history['RSI-AVG-U'] / coin_history['RSI-AVG-D']
+
+	# EMA (EXPONENTIAL MOVING AVERAGE)
+	# EMA14 (Price(today) × a + EMA(yesterday) × (1 − a))
+	elif algorithm == 'ema':
+		a = 2 / (N + 1)
+		coin_history['RSI-AVG-U'], coin_history['RSI-AVG-D'] = [0, 0]
+		coin_history['RSI-AVG-U'] = coin_history['U'] * a + coin_history['RSI-AVG-U'].shift() * (1 - a)
+		coin_history['RSI-AVG-D'] = coin_history['D'] * a + coin_history['RSI-AVG-D'].shift() * (1 - a)
+		coin_history['RSI-RS'] = coin_history['RSI-AVG-U'].rolling(window=N).mean() / coin_history['RSI-AVG-D'].rolling(window=N).mean()
+
+	# WSM14 (WILDER’S SMOOTHING METHOD)
+	elif algorithm == 'wsm':
+		coin_history['RSI-AVG-U'], coin_history['RSI-AVG-D'] = [0, 0]
+		coin_history['RSI-AVG-U'] = coin_history['U'] * (1 / N) + coin_history['RSI-AVG-U'].shift() * ((N - 1) / N)
+		coin_history['RSI-AVG-D'] = coin_history['D'] * (1 / N) + coin_history['RSI-AVG-D'].shift() * ((N - 1) / N)
+		coin_history['RSI-RS'] = coin_history['RSI-AVG-U'].rolling(window=N).mean() / coin_history['RSI-AVG-D'].rolling(window=N).mean()
+
+	coin_history['RSI'] = 100 - 100 / (1 + coin_history['RSI-RS'])
 
 	return coin_history
